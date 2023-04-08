1846f5753a379363b49a2eccb5314f69cf4593dd
rikzyj <108289652+rikzyj@users.noreply.github.com>
2023-04-08 22:33:14 +0530

    Music Recommendation Engine

--- Music Genre Classification/MusicRecommendation.py
+++ Music Genre Classification/MusicRecommendation.py
@@ -10,6 +10,7 @@ SPOTIPY_CLIENT_SECRET = 'f4f12b9755114eb78f5408499c02cd1d'
 client_credentials_manager = SpotifyClientCredentials(client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
+
 def get_top_10_trending_songs(genre):
     # Get the Spotify category ID for the genre
     categories = sp.categories(limit=50)['categories']['items']
@@ -43,12 +44,11 @@ def get_top_10_trending_songs(genre):
 
     return trending_songs
 
+
 if __name__ == "__main__":
-    genre = "jazz"
+    genre = "pop"
     trending_songs = get_top_10_trending_songs(genre)
     if trending_songs:
         print(f"Top 10 trending songs in {genre} genre:")
         for idx, song in enumerate(trending_songs, start=1):
             print(f"{idx}. {song}")
-
-
